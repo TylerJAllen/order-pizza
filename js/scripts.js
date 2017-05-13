0aa062bcad36d4f090fa0d0f18dfe345caa4b96a
Tyler Allen <mynameistylerallen@gmail.com>
2017-05-13 00:24:04 -0700

    Add options for sauce, veggies, sides, and drinks. Make adjustments to js/scripts.js to get full functionality and made adjustments to css/scripts.css.

--- js/scripts.js
+++ js/scripts.js
@@ -1,79 +1,141 @@
 //-------------Business Logic-------------//
-
-//Constructor
-function Pizza(size, cheeses, meats) {
-  this.size = size;
-  this.cheeses = cheeses;
-  this.meats = meats;
+//Order constructor
+function Order(sides, drinks) {
+  this.pizza = [];
+  this.sides = sides;
+  this.drinks = drinks;
   this.price = 0;
+}
+
+//Pizza constructor that will go inside Order constructor
+function Pizza(size, sauces) {
+  this.size = size;
+  this.sauces = sauces;
+  this.cheeses = [];
+  this.meats = [];
+  this.veggies = [];
+}
+
+Pizza.prototype.outputPizzaDetails = function(){
+  $("ul#resultsSize").append("<li>" + this.size + "</li>");
+  $("ul#resultsSauce").append("<li>" + this.sauces + "</li>");
+  for(var index = 0; index < this.cheeses.length; index += 1){
+    $("ul#resultsCheeses").append("<li>" + this.cheeses[index] + "</li>");
+  }
+
+  for(var index = 0; index < this.meats.length; index += 1){
+    $("ul#resultsMeats").append("<li>" + this.meats[index] + "</li>");
+  }
+
+  for(var index = 0; index < this.veggies.length; index += 1){
+    $("ul#resultsVeggies").append("<li>" + this.veggies[index] + "</li>");
+  }
 }
 
 //Prototype that calculates price of pizza based on user inputs
 Pizza.prototype.pizzaPrice = function() {
+  var price = 0;
   //Determine price based on user selected size
   if (this.size === "Small"){
-    this.price += 4;
+    price += 4;
   }
   else if (this.size === "Medium"){
-    this.price += 6;
+    price += 6;
   }
   else if (this.size === "Large"){
-    this.price += 8;
+    price += 8;
   }
   //Determine price based on user selected cheese(s)
   if (this.cheeses.length === 5){
-    this.price += 2;
+    price += 2;
   }
   else if (this.cheeses.length === 4){
-    this.price += 1.50;
+    price += 1.50;
   }
   else if (this.cheeses.length === 3){
-    this.price += 1;
+    price += 1;
   }
   else if (this.cheeses.length === 2){
-    this.price += .60;
+    price += .60;
   }
   else if (this.cheeses.length === 1){
-    this.price += .30;
+    price += .30;
   }
   //Determine price based on user selected meat(s)
-  if (this.meats.length === 8) {
-    this.price += 8;
-  }
-  else if (this.meats.length === 7) {
-    this.price += 6.50;
-  }
   else if (this.meats.length === 6) {
-    this.price += 5;
+    price += 5;
   }
   else if (this.meats.length === 5) {
-    this.price += 4;
+    price += 4;
   }
   else if (this.meats.length === 4) {
-    this.price += 3;
+    price += 3;
   }
   else if (this.meats.length === 3) {
-    this.price += 2.25;
+    price += 2.25;
   }
   else if (this.meats.length === 2) {
-    this.price += 1.50;
+    price += 1.50;
   }
   else if (this.meats.length === 1) {
-    this.price += .75;
+    price += .75;
   }
+  //Determine price based on user selected veggie(s)
+  if (this.veggies.length > 5) {
+    price += 3;
+  }
+  else if (this.veggies.length > 2) {
+    price += 1.5;
+  }
+  return price;
 }
 
+//Prototype that inputs pizza price and calculates total price of user order
+Order.prototype.orderPrice = function() {
+  //Determine price based on user selected side(s)
+  if(this.sides.includes("Breadsticks") === true) {
+    this.price += 3;
+  }
+  if (this.sides.includes("Cheesy Breadsticks") === true) {
+    this.price += 4;
+  }
+  if (this.sides.includes("Pasta - Chicken Alfredo") === true) {
+    this.price += 5;
+  }
+  if (this.sides.includes("Pasta - Meaty Marinara") === true) {
+    this.price += 5;
+  }
+  if (this.sides.includes("Wings - Traditional") === true) {
+    this.price += 6;
+  }
+  if (this.sides.includes("Wings - BBQ") === true) {
+    this.price += 6;
+  }
+  //Determine price based on user selected drink(s)
+  if(this.drinks.length === 4){
+    this.price += 6;
+  }
+  else if(this.drinks.length === 3){
+    this.price += 4.5;
+  }
+  else if(this.drinks.length === 2){
+    this.price += 3;
+  }
+  else if(this.drinks.length === 1){
+    this.price += 1.5;
+  }
+}
 
 //-------------User Interface Logic-------------//
 $(function(){
   //User Interface Global Variables
   var userSize = "";
-  // var userSauce = "";
+  var userSauce = "";
   var userCheeses = [];
   var userMeats = [];
-  // var userVeggies = "";
-  // var userSides = "";
-  // var userDrinks = "";
+  var userVeggies = [];
+  var userSides = [];
+  var userDrinks = [];
 
   $(".formOne").click(function(event){
     event.preventDefault();
@@ -83,22 +145,25 @@ $(function(){
 
   $(".formSize").submit(function(event){
     event.preventDefault();
-    userSize = $("input:radio[name=pizzaSize]:checked").val();
+    userSize = $("input:radio[name=size]:checked").val();
     $(".formSize").hide();
+    $(".formSauce").show();
+  });
+
+  $(".formSauce").submit(function(event){
+    event.preventDefault();
+    userSauce = $("input:radio[name=sauce]:checked").val();
+    //Create newPizza constructor with user inputs
+    newPizza = new Pizza(userSize, userSauce);
+    $(".formSauce").hide();
     $(".formCheese").show();
   });
 
-  // $(".formSauce").submit(function(event){
-  //   event.preventDefault();
-  //
-  // });
-
   $(".formCheese").submit(function(event){
     event.preventDefault();
-    //Populate userCheeses array with user cheese inputs
     $("input:checkbox[name=cheese]:checked").each(function(){
       var cheese = $(this).val();
-      userCheeses.push(cheese);
+      newPizza.cheeses.push(cheese);
     });
     $(".formCheese").hide();
     $(".formMeat").show();
@@ -106,44 +171,74 @@ $(function(){
 
   $(".formMeat").submit(function(event){
     event.preventDefault();
-    //Populate userMeats array with user meats inputs
     $("input:checkbox[name=meats]:checked").each(function(){
       var meat = $(this).val();
-      userMeats.push(meat);
+      newPizza.meats.push(meat);
     });
-
-    var newPizza = new Pizza(userSize, userCheeses, userMeats);
-
-    //call pizzaPrice() method to calculate price of user created pizza
-    newPizza.pizzaPrice();
-    $("#totalPrice").text("$" + newPizza.price.toFixed(2))
-    $("ul").append("<li>" + newPizza.size + "</li>");
-    $("ul").append("<li>" + newPizza.cheeses + "</li>");
-    $("ul").append("<li>" + newPizza.meats + "</li>");
     $(".formMeat").hide();
-    $(".results").show();
+    $(".formVeggies").show();
   });
 
-  // $(".formVeggies").submit(function(event){
-  //   event.preventDefault();
-  //
-  //
-  // });
-  //
-  // $(".formSides").submit(function(event){
-  //   event.preventDefault();
-  //
-  //
-  // });
-  //
-  // $(".formDrinks").submit(function(event){
-  //   event.preventDefault();
-  //
-  //
-  // });
+  $(".formVeggies").submit(function(event){
+    event.preventDefault();
+    $("input:checkbox[name=veggies]:checked").each(function(){
+      var veggie = $(this).val();
+      newPizza.veggies.push(veggie);
+    });
+    $(".formVeggies").hide();
+    $(".formSides").show();
+  });
 
-  //Create constructor with user inputs
+  $(".formSides").submit(function(event){
+    event.preventDefault();
+    $("input:checkbox[name=sides]:checked").each(function(){
+      var sides = $(this).val();
+      userSides.push(sides);
+    });
+    $(".formSides").hide();
+    $(".formDrinks").show();
+  });
+
+  $(".formDrinks").submit(function(event){
+    event.preventDefault();
+    $("input:checkbox[name=drinks]:checked").each(function(){
+      var drinks = $(this).val();
+      userDrinks.push(drinks);
+    });
+
+    //Create newOrder constructor with user inputs
+    var newOrder = new Order(userSides, userDrinks);
+
+    //Push newPizza constructor to pizza array in newOrder constructor
+    newOrder.pizza.push(newPizza);
+
+    //Call pizzaPrice() method to calculate price of user created pizza and store as newOrder.price
+    newOrder.price = newPizza.pizzaPrice();
+
+    //Call orderPrice() method to calculate total price of order
+    newOrder.orderPrice();
+
+    //Output total price and order details to screen
+    $("#totalPrice").text(newOrder.price.toFixed(2));
+    // $("ul").append("<li>" + newPizza + "</li>");
 
 
 
+    newOrder.pizza.forEach(function(input){
+      input.outputPizzaDetails();
+    });
+
+    for(var index = 0; index < newOrder.sides.length; index += 1){
+      $("ul#resultsSides").append("<li>" + newOrder.sides[index] + "</li>");
+    };
+
+    for(var index = 0; index < newOrder.drinks.length; index += 1){
+      $("ul#resultsDrinks").append("<li>" + newOrder.drinks[index] + "</li>");
+    };
+
+    // $("ul").append("<li>" + newOrder.sides + "</li>");
+    // $("ul").append("<li>" + newOrder.drinks + "</li>");
+    $(".formDrinks").hide();
+    $(".results").show();
+  });
 });
